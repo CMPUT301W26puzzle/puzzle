c5c333fce0bb89e31a16e8069ea7b4d6a99b2361
Zeemo10 <zeemeau@gmail.com>
2026-01-29 18:18:10 -0700

    changed pentagon class

--- app/src/main/java/com/example/cmput301_lab_04/Pentagon.java
+++ app/src/main/java/com/example/cmput301_lab_04/Pentagon.java
@@ -1,7 +1,6 @@
 package com.example.cmput301_lab_04;
 
 public class Pentagon extends Shape {
-    int x, y;
-    double side;
-    double area;
+    private double side;
+    private double area;
 }
